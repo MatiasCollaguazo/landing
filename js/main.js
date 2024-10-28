840e57d39463d18d82aa78005489ce3aea2a9681
MatiasCollaguazo <152077122+MatiasCollaguazo@users.noreply.github.com>
2024-10-28 14:54:34 -0500

    push :D

--- js/main.js
+++ js/main.js
@@ -1,27 +1,67 @@
+// Reemplaza con tu URL
+const databaseURL = 'https://landing-57dbb-default-rtdb.firebaseio.com/coleccion.json';
+
+
+let sendData = () => {
+    // Obtén los datos del formulario
+    const form = document.getElementById("form");
+    const emailElement = document.querySelector(".form-control-lg")
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+            return response.json(); // Procesa la respuesta como JSON
+        })
+        .then(result => {
+            alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+            emailElement.classList.add('input-sended');
+            setTimeout(() => {
+                emailElement.classList.remove('input-sended');
+            }, 5000)
+            form.reset()
+
+            
+        })
+        .catch(error => {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        });
+}
+
 let ready = () => {
     //console.log('DOM está listo')
 }
 
 let loaded = (eventLoaded) => {
-    let myform = document.getElementById("form");
+    const myform = document.getElementById("form");
     console.log(myform)
-    myform.addEventListener('submit', (eventSubmit)=>{
+    myform.addEventListener('submit', (eventSubmit) => {
         eventSubmit.preventDefault();
         const emailElement = document.querySelector(".form-control-lg")
         const emailText = emailElement.value;
-        if (emailText.length===0) {
+        if (emailText.length === 0) {
             emailElement.classList.add('input-error');
             emailElement.focus();
             emailElement.animate([
-            { transform: 'translateX(0)' },    // Inicio en 0px
-            { transform: 'translateX(8px)' }, // Mover a 50px
-            { transform: 'translateX(-8px)' },// Mover a -50px
-            { transform: 'translateX(0)' }     // Volver a 0px
+                { transform: 'translateX(0)' },    // Inicio en 0px
+                { transform: 'translateX(8px)' }, // Mover a 50px
+                { transform: 'translateX(-8px)' },// Mover a -50px
+                { transform: 'translateX(0)' }     // Volver a 0px
             ], {
-            
-            duration: 300,
-            iterations: 1,
-            easing: 'linear'
+
+                duration: 300,
+                iterations: 1,
+                easing: 'linear'
             });
             emailElement.addEventListener('input', () => {
                 if (emailElement.value.length > 0) {
@@ -29,13 +69,16 @@ let loaded = (eventLoaded) => {
                 }
             });
             emailElement.addEventListener('blur', () => {
-                setTimeout(()=>{
+                setTimeout(() => {
                     emailElement.classList.remove('input-error');
-                },5000)
+                }, 5000)
             });
+            return;
         }
+        sendData();
+        return;
     });
-    
+
 }
 
 window.addEventListener("DOMContentLoaded", ready);
